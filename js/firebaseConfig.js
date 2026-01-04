a7a4aa910245e424a8c7acfce6428a3e95550377
basitazizi <112999295+basitazizi@users.noreply.github.com>
2026-01-04 00:02:42 -0800

    update and push all

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+// Firebase configuration file
+// Replace these values with your Firebase project's config values.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCYgQjXfYYQ_GgaA7u_WbjDXhwlRxHRQ_8",
+  authDomain: "debtpilot-5d906.firebaseapp.com",
+  projectId: "debtpilot-5d906",
+  storageBucket: "debtpilot-5d906.appspot.com",
+  messagingSenderId: "263058954314",
+  appId: "1:263058954314:web:07ebbed42d2b5309092eb3"
+};
